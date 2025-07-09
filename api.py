56ebe713cc1436f1b9b94dfeafa0e589d8870808
Rafaello85 <r.borek@wp.pl>
2025-07-09 18:43:42 +0200

    first commit

---
+++ api.py
@@ -0,0 +1,29 @@
+from config import Settings
+import requests
+import datetime
+
+
+def fetch_weather ():
+    URL = f"https://api.openweathermap.org/data/2.5/weather?q={Settings.city}&appid={Settings.api_key}"
+
+    try:
+        response = requests.get(URL)
+        weather = response.json()
+
+        data = {
+            "Odczuwalna": weather["main"]["feels_like"],
+            "Ciśnienie": weather["main"]["pressure"],
+            "Wilgotność": weather["main"]["humidity"],
+            "Zwykla temperatura": weather["main"]["temp"],
+            "Opis pogody": weather["weather"][0]["description"],
+            "Miejsce": weather["name"],
+            "Prędkosc wiatru": weather["wind"]["speed"],
+            "Data pomiaru": datetime.datetime.now()
+        }
+        return data
+
+    except Exception as e:
+        print (e)
+
+
+fetch_weather()
